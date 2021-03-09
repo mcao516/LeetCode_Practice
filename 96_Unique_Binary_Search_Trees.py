64268dc6589b7327b9c54d35457c694fb7e3920a
mcao610 <c_meng@outlook.com>
2021-03-09 18:11:55 -0500

    add new solution for #96

--- 96_Unique_Binary_Search_Trees.py
+++ 96_Unique_Binary_Search_Trees.py
@@ -11,3 +11,15 @@ class Solution:
     
             self.DP[n] = total
             return self.DP[n]
+
+
+class Solution2:
+    def numTrees(self, n: int) -> int:
+        F = [0] * (n + 1)
+        F[0], F[1] = 1, 1
+        
+        for i in range(2, n + 1):
+            for j in range(i):
+                F[i] += F[j] * F[i - 1 - j]
+        
+        return F[n]
