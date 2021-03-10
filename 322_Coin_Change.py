29edef03e64073bc47234c14e322c2f1ca5c91ac
mcao610 <c_meng@outlook.com>
2021-03-09 23:07:17 -0500

    update coin change

--- 322_Coin_Change.py
+++ 322_Coin_Change.py
@@ -1,24 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         """
-        F(i) = min(F(i - c) for every c) + 1
+        F[i] = F[i-c] for every coin
         
         """
         F = [0] * (amount + 1)
-        min_value = min(coins)
         
-        F[0] = 0
-        for i in range(1, amount + 1):
-            nums = []
-            if i < min_value:  # no coin can further break
-                nums.append(1e5)
-            
+        for a in range(1, amount + 1):
+            cuttable = [1e5]
             for c in coins:
-                if i >= c:
-                    nums.append(F[i-c])
-            F[i] = min(nums) + 1
+                if a >= c:
+                    cuttable.append(F[a-c])
+            F[a] = min(cuttable) + 1
         
         if F[amount] >= 1e5:
             return -1
-        else:
-            return F[amount]
+        return F[amount]
