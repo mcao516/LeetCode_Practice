f8ba7873a03c93e371bc25d1a55566234bcc34dd
mcao610 <c_meng@outlook.com>
2021-03-12 12:41:27 -0500

    add recursive solution to #617

--- 617_Merge_Two_Binary_Trees.py
+++ 617_Merge_Two_Binary_Trees.py
@@ -7,6 +7,20 @@
 
 class Solution:
     def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if root1 is None:
+            return root2
+        if root2 is None:
+            return root1
+        
+        merged = TreeNode(root1.val + root2.val)
+        merged.left = self.mergeTrees(root1.left, root2.left)
+        merged.right = self.mergeTrees(root1.right, root2.right)
+        
+        return merged
+
+
+class Solution2:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
         if root1 is None and root2 is None:
             return None
         if root1 and root2 is None:
