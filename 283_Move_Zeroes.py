8c90eb807fa6581aeaa35867ff322d4b5e1d0b3f
mcao610 <c_meng@outlook.com>
2021-03-16 22:15:47 -0400

    add #283

---
+++ 283_Move_Zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pointer = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pointer], nums[i] = nums[i], nums[pointer]
+                pointer += 1
+                
+        return nums
