4184262a8031bc6a9ccdfb1f3b0ea4bc7a3af56a
mcao610 <c_meng@outlook.com>
2021-03-16 23:36:07 -0400

    add iterative solution to #70

--- 70_Climbing_Stairs.py
+++ 70_Climbing_Stairs.py
@@ -8,3 +8,17 @@ class Solution:
             if n - 1 not in self.K:
                 self.K[n-1] = self.climbStairs(n-1)
             return self.K[n-1] + self.K[n-2]
+
+
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        """
+        F[i] = F[i-1] + F[i-2]
+        
+        """
+        F = [1] * (n + 1)
+        
+        for i in range(2, n + 1):
+            F[i] = F[i-1] + F[i-2]
+        
+        return F[n]
