a351553435ef49265380b79499e05c2042b1ea12
mcao610 <c_meng@outlook.com>
2021-03-16 22:27:50 -0400

    update #21

--- 21_Merge_two_sorted_lists.py
+++ 21_Merge_two_sorted_lists.py
@@ -8,33 +8,33 @@ class Solution:
     def mergeTwoLists(self, l1, l2):
         if l1 is None and l2 is None:
             return None
-        if l1 is None: return l2
-        if l2 is None: return l1
+        elif l1 is None and l2 is not None:
+            return l2
+        elif l1 is not None and l2 is None:
+            return l1
         
-        pointer_1, pointer_2 = l1, l2
-        if l1.val <= l2.val:
-            sorted_list = l1
-            pointer_1 = pointer_1.next
-        else:
-            sorted_list = l2
-            pointer_2 = pointer_2.next
-        sorted_list.next = None
-        origin_pointer = sorted_list
+        start = curr = ListNode(val=-1)
         
-        while pointer_1 and pointer_2:
-            if pointer_1.val <= pointer_2.val:
-                sorted_list.next = pointer_1
-                sorted_list = sorted_list.next
-                pointer_1 = pointer_1.next
+        while l1 and l2:
+            if l1.val > l2.val:
+                curr.next = l2
+                curr = curr.next
+                l2 = l2.next
             else:
-                sorted_list.next = pointer_2
-                sorted_list = sorted_list.next
-                pointer_2 = pointer_2.next
+                curr.next = l1
+                curr = curr.next
+                l1 = l1.next
                 
-        if pointer_2 is None:
-            sorted_list.next = pointer_1
-        elif pointer_1 is None:
-            sorted_list.next = pointer_2
+        while l1:
+            curr.next = l1
+            curr = curr.next
+            l1 = l1.next
             
-        return origin_pointer
+        while l2:
+            curr.next = l2
+            curr = curr.next
+            l2 = l2.next
+            
+        return start.next
     
+
