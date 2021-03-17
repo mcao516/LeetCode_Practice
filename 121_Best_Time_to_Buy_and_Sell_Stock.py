cce62f787cf17a342a286af1a2f8528c15a3bdb0
mcao610 <c_meng@outlook.com>
2021-03-16 23:02:03 -0400

    update 3121

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,19 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # K(i) = (K(i+1) > 0 ? K(i+1) : 0) + (A[i+1] - A[i])
+        """
+        F[i] = max(prices[i] - prices[i-1], prices[i] - prices[i-1] + F[i-1])
+        
+        """
         n = len(prices)
         F = [0] * n
         
-        F[n-1] = 0
-        F[n-2] = prices[n-1] - prices[n-2]
-        
-        for i in range(2, n):
-            if F[n-i] > 0:
-                F[n-i-1] = F[n-i] + prices[n-i] - prices[n-i-1]
+        F[0] = 0
+        for i in range(1, n):
+            if F[i - 1] > 0:
+                F[i] = prices[i] - prices[i-1] + F[i-1]
             else:
-                F[n-i-1] = prices[n-i] - prices[n-i-1]
+                F[i] = prices[i] - prices[i-1]
                 
-        return max(0, max(F))
+        return max(F)
 
 
 class Solution2:
